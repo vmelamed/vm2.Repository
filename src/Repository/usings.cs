686a1e13a01731b347620eb07eeabb2c473cc500
Val Melamed <valo@vtmelamed.com>
2026-07-02 20:04:53 -0400

    chore: save WIP [skip ci]

--- src/Repository/usings.cs
+++ src/Repository/usings.cs
@@ -1,13 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025-2026 Val Melamed
 
-global using System;
-global using System.Collections.Generic;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Linq;
-global using System.Threading;
-global using System.Threading.Tasks;
-
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.ChangeTracking;
 global using Microsoft.EntityFrameworkCore.Infrastructure;
--- tests/Repository.Tests/usings.cs
+++ tests/Repository.Tests/usings.cs
@@ -1,14 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-global using System.Diagnostics.CodeAnalysis;
-global using System.Runtime.Serialization;
-global using System.Text;
-
 global using Microsoft.EntityFrameworkCore;
 
 global using Xunit.Abstractions;
-
-global using vm2.TestUtilities;
 
 global using static vm2.TestUtilities.TestUtilities;
 
